766fd9447ac3e160b7db47a96e0f76355f9d05d0
SofiMoguilner <smoguilnerribes@frba.utn.edu.ar>
2025-08-15 11:47:12 -0300

    teg

--- ejIntegradores-modulo1-5/teg.pl
+++ ejIntegradores-modulo1-5/teg.pl
@@ -58,3 +58,80 @@ objetivo(rojo, ocuparContinente(asia)).
 objetivo(azul, ocuparPaises([argentina, bolivia, francia, inglaterra, china])).
 objetivo(verde, destruirJugador(rojo)).
 objetivo(negro, ocuparContinente(europa)).
+
+% estaEnContinente/2: Relaciona un jugador y un continente si el jugador ocupa al 
+% menos un país en el continente.
+
+estaEnContinente(Jugador, Continente):-
+    ocupa(Pais,Jugador,_),
+    paisContinente(Pais,Continente).
+
+% cantidadPaises/2: Relaciona a un jugador con la cantidad de países que ocupa.
+ cantidadPaises(Jugaodor, CantPaises):-
+    findall(Pais,ocupa(Pais,Jugador,_),Paises),
+    length(Paises,CantPaises).
+
+% ocupaContinente/2: Relaciona un jugador y un continente si el jugador ocupa totalmente al continente.
+
+ocupaContinente(Jugador,Continente):-
+    continente(Continente).
+    forall(
+        paisContinente(Pais,Continente),
+        ocupa(Pais,Jugador,_)).
+
+% leFaltaMucho/2: Relaciona a un jugador y un continente si al jugador le falta ocupar más de 2 países de dicho continente.
+
+leFaltaMucho(Jugador,Continente):-
+    continente(Continente),
+    findall(Pais, (paisContinente(Pais,Continente),\+(ocupa(Pais,Jugador,_))),PasisesNoOCupa),
+    length(PasisesNoOCupa, Cant),
+    Cant > 2.
+
+% sonLimitrofes/2: Relaciona 2 países si son limítrofes.
+
+sonLimitrofes(P1,P2):-
+    limitrofes([P1,P2]).
+sonLimitrofes(P1,P2):-
+    limitrofes([P2,P1]).
+
+% esGroso/1: Un jugador es groso si cumple alguna de estas condiciones:
+   % ocupa todos los países importantes,
+   % ocupa más de 10 países
+   % o tiene más de 50 ejercitos.
+
+esGroso(Jugador):-
+    forall(paisImportante(Pais), ocupa(Pais,Jugador,_)).
+    
+esGroso(Jugador):-
+    findall(Pais, ocupa(Pais,Jugador,_),PaisesOcupados),
+    length(PaisesOcupados,Cant),
+    Cant > 10.
+esGroso(Jugador):-
+    findall(Ejercito, ocupa(_,Jugador,Ejercito),Ejercitos),
+    sum_list(Ejercitos, Suma),
+    Suma > 50.
+
+% estaEnElHorno/1: un país está en el horno si todos sus países limítrofes están ocupados 
+% por el mismo jugador que no es el mismo que ocupa ese país.
+
+estaEnElHorno(Pais):-
+    ocupa(Pais,Jugador,_),
+    forall((limitrofes(Pais,P1),  ocupa(Pais,OtroJugador,_)),(OtroJugador\=Jugador)).
+    
+% esCaotico/1: un continente es caótico si hay más de tres jugadores en él.
+
+esCaotico(Pais):-
+    findall(Jugador, ocupa(Pais,Jugador,_),ListaJugadores),
+    length(ListaJugadores,Cant),
+    Cant > 3.
+
+% capoCannoniere/1: es el jugador que tiene ocupado más países.
+
+capoCannoniere(Jugador):-
+    cantidadPaises(Jugador,CantMax),
+    forall((cantidadPaises(OtroJugador, OtraCant), Jugador \= OtroJugador),CantMax >= OtraCant).
+
+% ganadooor/1: un jugador es ganador si logro su objetivo 
+
+ganadooor(Jugador):-
+    
